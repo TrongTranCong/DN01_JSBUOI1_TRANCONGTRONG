f8f1f1dbb6eaeb8e302427780d415b3e41aea901
Trong <trongtran3010@gmail.com>
2021-07-25 19:16:26 +0700

    baitap2

---
+++ js/baitap2.js
@@ -0,0 +1,36 @@
+/**
+ * Khối 1: Input
+ * số thực
+ * num1, num2, num3, num4, num5
+ * 
+ * 
+ * Khối 2: Các bước xử lý
+ * B1: Tạo và gán giá trị cho 5 số thực
+ * B2: Tạo biến chứa số thực
+ * B3: Xây dựng công thức tính trung bình
+ * average = (num1+num2+num3+num4+num5)/5
+ * Chuyển sang lập trình:
+ * avg = sum()
+ * B4: Lấy độ dài từ công thức
+ * lấy căn bậc hai của cạnh huyền
+ * edge3 = math.sqrt(edge3 * edge3);
+ * B5: hiện kq lên console  
+ * 
+ * Khối 3: Output
+ * average(num1,..num5)
+ * console.log(average)
+ */
+
+// Tạo và gán giá trị
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+var num4 = 40;
+var num5 = 50;
+var sum = 0;
+var average = 0;
+
+// Công thức tính trung bình
+sum = num1 + num2 + num3 + num4 + num5
+average = sum / 5;
+console.log("Giá trị trung bình của 5 số:" + average)
