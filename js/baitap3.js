d63784a9654b5387be85e0a2416529f908a10ba6
Trong <trongtran3010@gmail.com>
2021-07-25 19:41:19 +0700

    baitap3

---
+++ js/baitap3.js
@@ -0,0 +1,29 @@
+/**
+ * Khối 1: Input
+ * 1 $ = 23500
+ * Số tiền($)
+ * amount, amount1
+ * 
+ * Khối 2: Các bước xử lý
+ * B1: Tạo và gán giá trị số tiền
+ * B2: Tạo biến chứa số tiền
+ * B3: Xây dựng công thức tính số tiền quy đổi
+ * amout 1$ = 23500 VND
+ * Chuyển sang lập trình:
+ * amount = amount 
+ * B4: Lấy độ dài từ công thức
+ * lấy căn bậc hai của cạnh huyền
+ * edge3 = math.sqrt(edge3 * edge3);
+ * B5: hiện kq lên console  
+ * 
+ * Khối 3: Output
+ * tính số tiền sau quy đổi
+ * 
+ */
+//  Tạo và gán giá trị số tiền
+var amountOfDollar = 23500;
+var amountDollar = 20;
+var amountExchangeVND = 0;
+// Viết công thức tính toán
+var amountExchangeVND = amountOfDollar * amountDollar;
+console.log("Số tiền sau quy đổi:"+ amountExchangeVND);
