f56e3b1e7a56d8fd2115f97944af00997ec2ae4c
Trong <>
2021-07-29 09:04:29 +0700

    add baitap1JSBuoi1

--- js/baitap1.js
+++ js/baitap1.js
@@ -1,27 +1,27 @@
 /**
  * Khối 1: Input
- * Lương 1 ngày: 100000
- * Số ngày làm
- * 
+ * Income a day: 100000
+ * Số ngày làm: days
+ * var Incomeaday, var days, var Income
  * 
  * Khối 2: Các bước xử lý
  * B1: Tạo và gán giá trị cho số ngày, tiền lương 1 ngày
  * B2: Tạo biến chứa số ngày, tiền lương
  * B3: Xây dựng công thức tính tiền lương ngày làm
- * Lương 1 ngày * số ngày làm = tổng tiền lương 
+ * Income a day * days = Income
  * Chuyển sang lập trình:
- * sumSal = salDay * day
+ * Income = Incomeaday * days
  * B4: hiện kết quả lên console  
  * 
  * Khối 3: Output
  * Tiền lương ngày làm
- * 
+ * Income
  */
 // Tạo và gán giá trị 
-var salDay = 100000;
-var day = 0;
+var Incomeaday = 100000;
+var days = 10;
 // Viết công thức tính toán
-sumSal = salDay * day;
+Income = Incomeaday * days;
 
 // Hiện kq lên console
-console.log("Tiền lương nhân viên:" + sumSal);
+console.log("Income:" + Income);
